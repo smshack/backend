da345e62293044eb9dcf3da72d9aad39f4b95fa8
smshack <5432tat@naver.com>
2022-09-07 22:43:58 +0900

    몽고 DB 연결 설정 yarn add mongoose

--- express-mongo/server.js
+++ express-mongo/server.js
@@ -2,15 +2,28 @@ const express = require("express");
 const cors = require('cors');
 const morgan = require("morgan");
 const helmet = require("helmet");
+const mongoose = require('mongoose');
 const app = express();
 const port = 31001;
 const appname = `mongo-app`
 
+const MONGO_URL = 'mongodb://127.0.0.1:27017/'
+const MONGODB_NAME = 'test'
 
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(morgan('combined'));
+
+mongoose.connect(`${MONGO_URL}${MONGODB_NAME}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+},(err) =>{
+    if (err) {
+      console.error("mongodb connection error", err);
+    }
+    console.log("mongodb connected");
+})
 
 
 app.use((req, res, next) => {
